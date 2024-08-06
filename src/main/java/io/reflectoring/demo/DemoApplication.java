44f5fd256a583434e1c1bd56353847b2d2cf17ca
abhaythakur9555 <abhaykumarthakur007@gmail.com>
2024-08-06 15:35:21 +0530

    Update DemoApplication.java

--- src/main/java/io/reflectoring/demo/DemoApplication.java
+++ src/main/java/io/reflectoring/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 	
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String home()
 	{
 		return "Hello java";
